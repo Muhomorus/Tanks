2d4a0008c3ea9a92a994b8968a488732fdf01490
Mu <we>
2022-01-25 00:27:48 +1000

    ltym lj,hsq

--- bb.py
+++ bb.py
@@ -10,18 +10,18 @@ from pvpend import pvp_end
 stand = 1
 mp1, mp2, level, score, win, winner = None, None, None, None, None, None
 while stand != 0:
-    if stand == 1:  #гл меню
+    if stand == 1:
         stand = start()
-    elif stand == 2:  # 1инра
+    elif stand == 2:
         stand, score, level, win = game(mp1, mp2, level)
-    elif stand == 3:  #два инра
+    elif stand == 3:
         stand, winner = pvp()
-    elif stand == 4:  #три игра
+    elif stand == 4:
         stand, m1, m2 = red()
         write_level(m1, m2)
-    elif stand == 5: #выбор уровня
+    elif stand == 5:
         stand, mp1, mp2, level = lvl()
-    elif stand == 6: #завершени1 1 игра
+    elif stand == 6:
         stand, mp1, mp2 = men(score, level, win)
-    elif stand == 7: #завершени2 2 игра
+    elif stand == 7:
         stand = pvp_end(winner)
